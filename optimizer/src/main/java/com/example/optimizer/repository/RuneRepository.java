391471949f9b7d43edbf787f58e41fa0d4510177
setiawancf <v-setiawan.candrafu@traveloka.com>
2022-08-26 13:30:02 +0700

    Delete optimizer directory

--- optimizer/src/main/java/com/example/optimizer/repository/RuneRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.optimizer.repository;
-
-import com.example.optimizer.model.Rune;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RuneRepository extends JpaRepository<Rune, Long> {
-
-}
